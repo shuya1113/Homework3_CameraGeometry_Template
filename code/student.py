63f937ff6b23e942f5123b07501f4779048b1f89
shuya1113 <71192575+shuya1113@users.noreply.github.com>
2022-03-10 16:44:14 -0500

    Update student.py

--- code/student.py
+++ code/student.py
@@ -47,10 +47,35 @@ def calculate_projection_matrix(image, markers):
     # TODO: Your code here #
     ########################
     # This M matrix came from a call to rand(3,4). It leads to a high residual.
-    print('Randomly setting matrix entries as a placeholder')
-    M = np.array([[0.1768, 0.7018, 0.7948, 0.4613],
-                  [0.6750, 0.3152, 0.1136, 0.0480],
-                  [0.1020, 0.1725, 0.7244, 0.9932]])
+    len=np.shape(points2d)[0]
+    ct = 0
+    A = np.zeros((2*len,11))
+    b = np.zeros((2*len,1))
+    for i in range(len):
+        X = points3d[i,0]
+        Y = points3d[i,1]
+        Z = points3d[i,2]
+        u = points2d[i,0]
+        v = points2d[i,1]
+        row1 = [X,Y,Z,1,0,0,0,0,-u*X,-u*Y,-u*Z]
+        row2 = [0,0,0,0,X,Y,Z,1,-v*X,-v*Y,-v*Z]
+        A[ct,:] = row1
+        b[ct,:] = u
+        ct = ct + 1
+        A[ct,:] = row2
+        b[ct,:] = v
+        ct = ct + 1  
+
+    vec = np.linalg.lstsq(A, b, rcond=None)[0]
+    A1 = np.transpose(vec[0:4])
+    B1 = np.transpose(vec[4:8])
+    C1 = vec[8:11]
+    C1 = np.append(C1, 1)
+
+    M = np.zeros((3, 4))
+    M[0,:] = A1
+    M[1,:] = B1
+    M[2,:] = C1
 
     return M
 
@@ -106,9 +131,26 @@ def estimate_fundamental_matrix(points1, points2):
     ########################
     # TODO: Your code here #
     ########################
+    len = min(np.shape(points1)[0],np.shape(points2)[0])
 
+    A = np.zeros((len,9))
+
+    for i in range(len):
+        u1 = points1[i,0]
+        v1 = points1[i,1]
+        u2 = points2[i,0]
+        v2 = points2[i,1]
+        row = [u1*u2,v1*u2,u2,u1*v2,v1*v2,v2,u1,v1,1]
+        A[i,:] = row
+    U, S, Vh = np.linalg.svd(A)
+    F_matrix = Vh[-1]
+    F_matrix = np.reshape(F_matrix, (3, 3))
+    U, S, Vh = np.linalg.svd(F_matrix)
+    S[-1] = 0
+    S = np.diagflat(S)
+    F_matrix = np.dot(np.dot(U,S),Vh) 
     # This is an intentionally incorrect Fundamental matrix placeholder
-    F_matrix = np.array([[0, 0, -.0004], [0, 0, .0032], [0, -0.0044, .1034]])
+    # F_matrix = np.array([[0, 0, -.0004], [0, 0, .0032], [0, -0.0044, .1034]])
 
     return F_matrix
 
@@ -173,5 +215,40 @@ def matches_to_3d(points1, points2, M1, M2):
     ########################
     # TODO: Your code here #
     ########################
+    len = np.shape(points1)[0]
+    points3d = np.zeros((len,3))
+    m11=M1[0,0]
+    m12=M1[0,1]
+    m13=M1[0,2]
+    m14=M1[0,3]
+    m21=M1[1,0]
+    m22=M1[1,1]
+    m23=M1[1,2]
+    m24=M1[1,3]
+    m31=M1[2,0]
+    m32=M1[2,1]
+    m33=M1[2,2]
+    m34=M1[2,3]
+    n11=M2[0,0]
+    n12=M2[0,1]
+    n13=M2[0,2]
+    n14=M2[0,3]
+    n21=M2[1,0]
+    n22=M2[1,1]
+    n23=M2[1,2]
+    n24=M2[1,3]
+    n31=M2[2,0]
+    n32=M2[2,1]
+    n33=M2[2,2]
+    n34=M2[2,3]
 
+    a=np.array([[m31-m11,m32-m12,m33-m13],[m31-m21,m32-m22,m33-m23],[n31-n11,n32-n12,n33-n13],[n31-n21,n32-n22,n33-n23]])
+    for i in range(len):
+        u1=points1[i,0]
+        v1=points1[i,1]
+        u2=points2[i,0]
+        v2=points2[i,1]
+        b=[m14-m34*u1,m24-m34*v1,n14-n34*u2,n24-n34*v2]
+        points3d[i,:] = np.linalg.lstsq(a, b,rcond=None)[0]      
+    points3d = points3d.tolist()
     return points3d
